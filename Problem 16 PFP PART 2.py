8b3a218173f8a98f89150bb050ca0cec1c5070cb
ayushsrv <119559274+ayushsrv@users.noreply.github.com>
2023-09-11 09:46:16 +0530

    Add files via upload

---
+++ Problem 16 PFP PART 2.py
@@ -0,0 +1,10 @@
+def rotate_list(input_list,n):
+    l = len(input_list)
+    m = l-n
+    output_list = input_list[m:] + input_list[:m]
+    return output_list
+
+
+input_list = [1,2,3,4,5,6]
+output_list = rotate_list(input_list,4)
+print(output_list)
